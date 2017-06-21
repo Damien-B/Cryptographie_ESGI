b3e8b53012b8bcb7fe6c5939be232ebbfb30ac29
Damien Bannerot <bannerotdamien@gmail.com>
2017-06-22 01:20:25 +0200

    add second XOR with key

--- tp2/cbc.c
+++ tp2/cbc.c
@@ -10,6 +10,7 @@ Comments :
 #include <string.h>
 #include <time.h>
 
+
 // function used to encrypt an array of char with a key
 void encryptDecryptWithXOR(char *input, char *output, char *keyToEncrypt, int length) {
   // printf("XOR input '%s' of length %lu\n", input, strlen(input));
@@ -18,7 +19,7 @@ void encryptDecryptWithXOR(char *input, char *output, char *keyToEncrypt, int le
   for(i=0;i<length;i++) {
     output[i] = (char)(input[i] ^ keyToEncrypt[i]);
   }
-  // printf("XOR output '%s' of length %lu\n", output, strlen(output));
+  printf("XOR output '%s' of length %lu\n", output, strlen(output));
 }
 
 // function used to encrypt a file
@@ -56,15 +57,22 @@ void encrypt(char *inputFileName, char *outputFileName, char *userKey) {
           break;
         }
       }
-      if(iterations == 0) {
+      if(iterations == 0) {// first bytes
         // first iteration : use the key as initialization vector
         encryptDecryptWithXOR(currentBytes, encryptedBytes, key, strlen(userKey));
+        // doing a XOR with the key
+        encryptDecryptWithXOR(encryptedBytes, encryptedBytes, key, strlen(userKey));
       } else if (endOfFilePadding == 0) {
         // encrypt with the last result we got
         encryptDecryptWithXOR(currentBytes, encryptedBytes, previousDigestedBytes, strlen(userKey));
-      } else {
+        // doing a XOR with the key
+        encryptDecryptWithXOR(encryptedBytes, encryptedBytes, key, strlen(userKey));
+      } else {// last bytes
+        printf("currentBytes : %s\n", currentBytes);
         // encrypt the end of the file (handling padding)
         encryptDecryptWithXOR(currentBytes, encryptedBytes, previousDigestedBytes, endOfFilePadding);
+        // doing a XOR with the key
+        encryptDecryptWithXOR(encryptedBytes, encryptedBytes, key, endOfFilePadding);
       }
       // write the bytes in the output file
       fputs(encryptedBytes, outputFile);
@@ -118,15 +126,22 @@ void decrypt(char *inputFileName, char *outputFileName, char *userKey) {
           break;
         }
       }
-      if(iterations == 0) {
+      if(iterations == 0) {// first bytes
         // first iteration : use the key as initialization vector
         encryptDecryptWithXOR(currentBytes, decryptedBytes, key, strlen(userKey));
+        // doing a XOR with the key
+        encryptDecryptWithXOR(decryptedBytes, decryptedBytes, key, strlen(userKey));
       } else if (endOfFilePadding == 0) {
         // decrypt with the last encrypted bytes
-        encryptDecryptWithXOR(currentBytes, decryptedBytes, previousBytes, strlen(userKey));
-      } else {
+        encryptDecryptWithXOR(currentBytes, decryptedBytes, key, strlen(userKey));
+        // doing a XOR with the key
+        encryptDecryptWithXOR(decryptedBytes, decryptedBytes, previousBytes, strlen(userKey));
+      } else {// last bytes
+        printf("currentBytes : %s\n", currentBytes);
         // decrypt the end of the file (handling padding)
-        encryptDecryptWithXOR(currentBytes, decryptedBytes, previousBytes, endOfFilePadding);
+        encryptDecryptWithXOR(currentBytes, decryptedBytes, key, endOfFilePadding);
+        // doing a XOR with the key
+        encryptDecryptWithXOR(decryptedBytes, decryptedBytes, previousBytes, endOfFilePadding);
       }
       // write the bytes in the output file
       fputs(decryptedBytes, outputFile);
@@ -163,8 +178,8 @@ int main() {
   }
   if(type == 'a') {
     // launch with test files
-    encrypt("test.txt", "testEncrypted", "aze");
-    decrypt("testEncrypted", "testDecrypted.txt", "aze");
+    encrypt("test.txt", "testEncrypted.txt", "PASS");
+    decrypt("testEncrypted.txt", "testDecrypted.txt", "PASS");
   } else {
     // user give the name of the input file
     printf("Enter the name of the file you want to %s (with the extension) :\n", type=='e'?"encrypt":"decrypt");
